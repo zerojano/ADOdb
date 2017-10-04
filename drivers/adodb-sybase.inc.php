8e57d3adcb53a10eabbdb17cd1ca8c5d55f4f796
Alejandro Aguayo <aaguayonitrigual@gmail.com>
2017-10-04 10:19:09 -0300

    Solución Warning Conction

--- drivers/adodb-sybase.inc.php
+++ drivers/adodb-sybase.inc.php
@@ -129,9 +129,9 @@ class ADODB_sybase extends ADOConnection {
 		}
 
 		if ($this->charSet) {
-			$this->_connectionID = sybase_connect($argHostname,$argUsername,$argPassword, $this->charSet);
+			$this->_connectionID = @sybase_connect($argHostname,$argUsername,$argPassword, $this->charSet);
 		} else {
-			$this->_connectionID = sybase_connect($argHostname,$argUsername,$argPassword);
+			$this->_connectionID = @sybase_connect($argHostname,$argUsername,$argPassword);
 		}
 
 		if ($this->_connectionID === false) return false;
